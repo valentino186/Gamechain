d7f6788a74d8dc4d8b7b243f1ae5f5f92495d4f6
valentino186 <52948605+valentino186@users.noreply.github.com>
2022-10-14 13:43:05 +0200

    LoginQueryValidator implementation

---
+++ Backend/GamechainApp/Gamechain.Application/Services/Authentication/Queries/Login/LoginQueryValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace Gamechain.Application.Services.Authentication.Queries.Login
+{
+    public class LoginQueryValidator : AbstractValidator<LoginQuery>
+    {
+        public LoginQueryValidator()
+        {
+            RuleFor(x => x.UserName)
+                .NotEmpty();
+
+            RuleFor(x => x.Password)
+                .NotEmpty();
+        }
+    }
+}
